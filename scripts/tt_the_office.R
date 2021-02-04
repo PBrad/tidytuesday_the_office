7b8c0248357fe3b9e38082b0c430c42f72aa4023
Patrick Bradshaw <31942325+PBrad@users.noreply.github.com>
2021-02-03 20:34:49 -0500

    plot ratings by episode and season

--- scripts/tt_the_office.R
+++ scripts/tt_the_office.R
@@ -8,6 +8,9 @@ library(schrute)
 library(dplyr)
 library(lubridate)
 library(ggplot2)
+theme_set(theme_minimal() + theme(panel.border = element_blank(),
+                                  panel.grid.major = element_blank(),
+                                  panel.grid.minor = element_blank()))
 
 # Load --------------------------------------------------------------------
 
@@ -25,10 +28,16 @@ nrow(distinct(df_transcripts, index)) # this is unique but just a row number
 nrow(distinct(df_transcripts, season, episode, character, text_w_direction)) # 54421 - almost!
                                                                              # character could repeat lines
 
+df_transcripts %>% 
+  group_by(season, episode, character, text_w_direction) %>% 
+  mutate(n = n()) %>% 
+  ungroup() %>% 
+  filter(n > 1) #  Repeated lines (e.g., "Hey." "What?" etc.)
+
 # Create separate ratings dataset -----------------------------------------
 
 # Streamline and de-dupe ratings / basic episode info
-df_ratings <- df %>% 
+df_ratings <- df_transcripts %>% 
   select(-c(index, character, text, text_w_direction))
 
 # De-dup at season-episode level. Dropping complete duplicates should be sufficient
@@ -62,3 +71,33 @@ count(df_ratings, air_date)
 
 df_ratings <- df_ratings %>% 
   mutate(air_date = as_date(air_date))
+
+# Plot
+df_ratings %>% 
+  ggplot(aes(x = air_date, y = imdb_rating)) +
+  geom_point() + 
+  geom_smooth(se = FALSE)
+
+# Mark the seasons
+df_ratings %>% 
+  mutate(season = as.character(season)) %>% 
+  ggplot(aes(x = air_date, y = imdb_rating, color = season)) +
+  geom_point() +
+  scale_color_brewer(type = 'qual', palette = 'Set1')
+
+df_ratings %>% 
+  group_by(season) %>% 
+  mutate(season_start = min(air_date))
+
+# TODO - do we need a zero-origin for y-axis?
+df_ratings %>% 
+  group_by(season) %>% 
+  mutate(season_start = min(air_date)) %>% 
+  ungroup() %>% 
+  ggplot(aes(x = air_date, y = imdb_rating, color = as.character(season))) +
+  geom_point() +
+  guides(color = FALSE) +
+  geom_vline(aes(xintercept = as.integer(season_start) - 10), size = 0.05) +
+  labs(x = "Air Date",
+       y = "IMDB Rating", 
+       title = "The Office - Ratings by Episode") 
